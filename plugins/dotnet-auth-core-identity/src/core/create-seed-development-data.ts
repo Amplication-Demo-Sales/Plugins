bea30b75b11beef4582b04d16f9993f6e1f55137
mulygottlieb <muly@amplication.com>
2024-09-16 11:58:39 +0300

    add dotnet plugins

---
+++ plugins/dotnet-auth-core-identity/src/core/create-seed-development-data.ts
@@ -0,0 +1,61 @@
+import { dotnetTypes } from "@amplication/code-gen-types";
+import { CodeBlock, CsharpSupport } from "@amplication/csharp-ast";
+import { getPluginSettings } from "../utils";
+
+export function CreateSeedDevelopmentDataBody(
+  resourceName: string,
+  context: dotnetTypes.DsgContext
+): CodeBlock {
+  const { seedUserEmail, seedUserPassword } = getPluginSettings(
+    context.pluginInstallations
+  );
+
+  return new CodeBlock({
+    references: [
+      CsharpSupport.classReference({
+        name: "Identity",
+        namespace: "Microsoft.AspNetCore.Identity",
+      }),
+      CsharpSupport.classReference({
+        name: "EntityFrameworkCore",
+        namespace: "Microsoft.AspNetCore.Identity.EntityFrameworkCore",
+      }),
+      CsharpSupport.classReference({
+        name: resourceName,
+        namespace: `${resourceName}.Infrastructure`,
+      }),
+    ],
+    code: `var context = serviceProvider.GetRequiredService<${resourceName}DbContext>();
+      var amplicationRoles = configuration
+          .GetSection("AmplicationRoles")
+          .AsEnumerable()
+          .Where(x => x.Value != null)
+          .Select(x => x.Value.ToString())
+          .ToArray();
+
+          var usernameValue = "${seedUserEmail}";
+          var passwordValue = "${seedUserPassword}";
+          var user = new IdentityUser
+          {
+              Email = usernameValue,
+              UserName = usernameValue,
+              NormalizedUserName = usernameValue.ToUpperInvariant(),
+              NormalizedEmail = usernameValue.ToUpperInvariant(),
+          };
+ 
+          var password = new PasswordHasher<IdentityUser>();
+          var hashed = password.HashPassword(user, passwordValue);
+          user.PasswordHash = hashed;
+          var userStore = new UserStore<IdentityUser>(context);
+          await userStore.CreateAsync(user);
+          var _roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+      
+          foreach (var role in amplicationRoles)
+          {
+              await userStore.AddToRoleAsync(user, _roleManager.NormalizeKey(role));
+          }
+      
+      
+      await context.SaveChangesAsync();`,
+  });
+}
---
+++ plugins/dotnet-db-sqlserver/src/index.ts
@@ -0,0 +1,106 @@
+import {
+  dotnetPluginEventsTypes,
+  dotnetPluginEventsParams as dotnet,
+  dotnetTypes,
+  FileMap,
+} from "@amplication/code-gen-types";
+import { ClassReference, CodeBlock, ProgramClass } from "@amplication/csharp-ast";
+import { updateDockerComposeProperties } from "./constants";
+import { getPluginSettings } from "./utils";
+import { pascalCase } from "pascal-case";
+import { join } from "path";
+
+const CONNECTION_STRING = "DefaultConnection";
+
+class MSSQLServerPlugin implements dotnetTypes.AmplicationPlugin {
+  register(): dotnetPluginEventsTypes.DotnetEvents {
+    return {
+      CreateServerDockerCompose: {
+        before: this.beforeCreateServerDockerCompose,
+      },
+      CreateServerCsproj: {
+        before: this.beforeCreateServerCsproj,
+      },
+      CreateServerAppsettings: {
+        before: this.beforeCreateServerAppsettings,
+      },
+      CreateProgramFile: {
+        after: this.afterCreateProgramFile,
+      },
+    };
+  }
+
+  beforeCreateServerDockerCompose(
+    context: dotnetTypes.DsgContext,
+    eventParams: dotnet.CreateServerDockerComposeParams
+  ) {
+    const settings = getPluginSettings(context.pluginInstallations);
+
+    eventParams.updateProperties.push(
+      ...updateDockerComposeProperties(settings)
+    );
+    return eventParams;
+  }
+
+  beforeCreateServerCsproj(
+    _: dotnetTypes.DsgContext,
+    eventParams: dotnet.CreateServerCsprojParams
+  ) {
+    eventParams.packageReferences.push({
+      include: "Microsoft.EntityFrameworkCore.SqlServer",
+      version: "8.0.5",
+    });
+
+    return eventParams;
+  }
+
+  beforeCreateServerAppsettings(
+    context: dotnetTypes.DsgContext,
+    eventParams: dotnet.CreateServerAppsettingsParams
+  ) {
+    const { port, password, user, host, dbName } = getPluginSettings(
+      context.pluginInstallations
+    );
+
+    eventParams.updateProperties = {
+      ...eventParams.updateProperties,
+      ConnectionStrings: {
+        [CONNECTION_STRING]: `Data Source=${host},${port};Initial Catalog=${dbName};User Id=${user};Password=${password};TrustServerCertificate=true;`,
+      },
+    };
+    return eventParams;
+  }
+
+  afterCreateProgramFile(
+    { resourceInfo, serverDirectories }: dotnetTypes.DsgContext,
+    eventParams: dotnet.CreateProgramFileParams,
+    programClass: FileMap<ProgramClass>
+  ): FileMap<ProgramClass> {
+    const serviceNamespace = pascalCase(resourceInfo?.name ?? "");
+    const serviceDbContext = `${pascalCase(resourceInfo?.name ?? "")}DbContext`;
+    const programCsPath = join(serverDirectories.srcDirectory, "Program.cs");
+    const programCs = programClass.get(programCsPath);
+    if (!programCs) 
+      return programClass;
+
+    programCs.code.builderServicesBlocks.push(
+      new CodeBlock({
+        code: `builder.Services.AddDbContext<${serviceDbContext}>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("${CONNECTION_STRING}")));`,
+        references: [
+          new ClassReference({
+            name: "AddDbContext",
+            namespace: "Microsoft.EntityFrameworkCore",
+          }),
+          new ClassReference({
+            name: serviceDbContext,
+            namespace: `${serviceNamespace}.Infrastructure`,
+          }),
+        ],
+      })
+    );
+    return programClass;
+
+  }
+}
+
+export default MSSQLServerPlugin;
